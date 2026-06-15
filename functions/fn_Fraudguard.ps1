38bfc85abd061ae874d59dc0f3eccc027f8fe4d2
scrawladmin <78272958+scrawladmin@users.noreply.github.com>
2026-06-15 01:49:36 -0600

    Add files via upload

--- functions/fn_Fraudguard.ps1
+++ functions/fn_Fraudguard.ps1
@@ -37,15 +37,97 @@ Function Get-fraudguardiprep {
         Try {
             if ($ipaddress -and $Headers) {
                 $response = Invoke-WebRequest -Uri "https://api.fraudguard.io/ip/$ipaddress" -Headers $Headers
+                # https://api.fraudguard.io/ip/$ipaddress
+                # Paid plan URL https://api.fraudguard.io/v2/ip/<IP>
             }
         }
         Catch {
-            Write-warning "$($_.Exception.Message)"
+            write-warning "$($_.Exception.Message)"
             return
         }
         if ($response) {
+            $apiname | Select-ColorString "FRAUDGUARD" -CaseSensitive -BackgroundColor $(Get-Random 'Gray','Blue','Green','Cyan','Red','Magenta','Yellow','White')
+            # $apiname | Trace-word -words 'FRAUDGUARD'
             $t = $response.Content | ConvertFrom-Json
-            $apiname | Trace-word -words 'FRAUDGUARD'
+            if ([switch]$raw) {
+                $t
+            }
+            Else {
+                $properties = ($t | Get-Member -MemberType Properties).Name
+                [hashtable]$table = @{
+                    PSTypeName = "FRAUDGUARD"
+                }
+                ForEach ($property in $properties) {
+                    If ($t."$property" -ne $t.location) {
+                        If ($t."$property") {
+                            $n = $property + ": " + $t."$property" 
+                            $table.Add($property, $t."$property")
+                            Write-log " [FRAUDGUARD] $n"
+                        }
+                    }
+                }
+            }
+        }
+    }
+    End {
+        if ($table) {
+            $htable = New-Object -TypeName psobject -Property $table
+            $htable | Format-List
+        }
+        Write-Verbose "Exiting $($MyInvocation.Mycommand)"
+    }
+}
+Function Get-fraudguardhostnamerep {
+    [cmdletbinding()]
+    [OutputType("hostnamerep")]
+    param(
+        [parameter(Position = 0,
+            Mandatory,
+            HelpMessage = "Enter an Hostanme to lookup with Fraudguard",
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        # [ValidatePattern("^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$")]
+        $hostname
+    )
+    Begin {
+        Write-Verbose "Function: $($MyInvocation.Mycommand)"
+        $apiname = 'FRAUDGUARD'
+        if ($Fraudguardkey) {
+            if ($fraudguarduserid) {
+                $pair = "$($fraudguarduserid):$($Fraudguardkey)"
+                $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+                $basicAuthValue = "Basic $encodedCreds"
+                $Headers = @{
+                    Authorization = $basicAuthValue
+                }  
+            }
+            Else {
+                write-warning "Requires Fraudguard User" -InformationAction Continue
+                return
+            } 
+        }
+        Else {
+            write-warning "Requires Fraudguard Pass" -InformationAction Continue
+            return
+        }
+    }
+    Process {
+        Try {
+            if ($hostname -and $Headers) {
+                $response = Invoke-WebRequest -Uri "https://api.fraudguard.io/v2/hostname/$hostname" -Headers $Headers
+                # Paid plan URL 
+                # https://api.fraudguard.io/v2/hostname/<hostname>
+            }
+        }
+        Catch {
+            write-warning "$($_.Exception.Message)"
+            return
+        }
+        if ($response) {
+            $apiname | Select-ColorString "FRAUDGUARD" -CaseSensitive -BackgroundColor $(Get-Random 'Gray','Blue','Green','Cyan','Red','Magenta','Yellow','White')
+            #$apiname | Trace-word -words 'FRAUDGUARD'
+            $t = $response.Content | ConvertFrom-Json
             if ([switch]$raw) {
                 $t
             }
--- functions/fn_WhoAPI.ps1
+++ functions/fn_WhoAPI.ps1
@@ -14,15 +14,16 @@ Function Get-domainscore {
         Write-Verbose "Function: $($MyInvocation.Mycommand)"
         $apiname = 'WHOAPI'
         $domain = $fqdn
-        $r         = "domainscore"
+        $r = "domainscore"
     }
     Process {
         if ($fqdn) {
             Try {
                 
-                $apikey    = "$whoapikey"
-                
-                $url = "https://api.whoapi.com/?domain=$domain&r=$r&apikey=$apikey"
+                $apikey = "$whoapikey"
+                # https://api.whoapi.com/?apikey=YOUR_API_KEY&r=domainscore&domain=whoapi.com
+                $url = "https://api.whoapi.com/?apikey=$apikey&r=$r&domain=$domain"
+                #$url = "https://api.whoapi.com/?domain=$domain&r=$r&apikey=$apikey"
                 $response = Invoke-WebRequest -Method Get -Uri "$url" -ContentType 'application/json'
             }
             Catch {
@@ -30,12 +31,25 @@ Function Get-domainscore {
             }
             if ($response) {
                 $t = $response.Content | ConvertFrom-Json
+                $script:tx = $t.results
                 $apiname | Trace-word -words 'WHOAPI'
                 if ([switch]$raw) {
                     $t
                 }
                 Else {
-                    
+                    $properties = ($t | Get-Member -MemberType Properties).Name
+                    [hashtable]$table = @{
+                        PSTypeName = "WHOAPI"
+                    }
+                    ForEach ($property in $properties) {
+                       #If ($t."$property" -eq $t.results) {
+                            If ($t."$property") {
+                                $n = $property + ": " + $t."$property" 
+                                $table.Add($property, $t."$property")
+                                Write-log " [WHOAPI] $n"
+                            }
+                       #}
+                    }
                 }
             }
         }
@@ -45,6 +59,205 @@ Function Get-domainscore {
             $htable = New-Object -TypeName psobject -Property $table
             $htable | Format-List
         }
+        $tx
+        Write-log " [WHOAPI] $($tx)"
+        Write-Verbose "Exiting $($MyInvocation.Mycommand)"
+    }
+}
+Function Get-domainscorecheck {
+    [cmdletbinding()]
+    [OutputType("domainscorecheck")]
+    param(
+        [parameter(Position = 0,
+            Mandatory,
+            HelpMessage = "Enter an FQDN address to lookup with domainscore",
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        $fqdn
+    )
+    begin {
+        Write-Verbose "Function: $($MyInvocation.Mycommand)"
+        $apiname = 'WHOAPI'
+        $domain = $fqdn
+        $r = "domainscore-check"
+    }
+    Process {
+        if ($fqdn) {
+            Try {
+                
+                $apikey = "$whoapikey"
+                # https://api.whoapi.com/?apikey=YOUR_API_KEY&r=domainscore&domain=whoapi.com
+                $url = "https://api.whoapi.com/?apikey=$apikey&r=$r&domain=$domain"
+                #$url = "https://api.whoapi.com/?domain=$domain&r=$r&apikey=$apikey"
+                $response = Invoke-WebRequest -Method Get -Uri "$url" -ContentType 'application/json'
+            }
+            Catch {
+                Write-error "$($_.Exception.Message)" 
+            }
+            if ($response) {
+                $t = $response.Content | ConvertFrom-Json
+                $script:tx = $t.results
+                $apiname | Trace-word -words 'WHOAPI'
+                if ([switch]$raw) {
+                    $t
+                }
+                Else {
+                    $properties = ($t | Get-Member -MemberType Properties).Name
+                    [hashtable]$table = @{
+                        PSTypeName = "WHOAPI"
+                    }
+                    ForEach ($property in $properties) {
+                       #If ($t."$property" -eq $t.results) {
+                            If ($t."$property") {
+                                $n = $property + ": " + $t."$property" 
+                                $table.Add($property, $t."$property")
+                                Write-log " [WHOAPI] $n"
+                            }
+                       #}
+                    }
+                }
+            }
+        }
+    }
+    End {
+        if ($table) {
+            $htable = New-Object -TypeName psobject -Property $table
+            $htable | Format-List
+        }
+        $tx
+        Write-log " [WHOAPI] $($tx)"
+        Write-Verbose "Exiting $($MyInvocation.Mycommand)"
+    }
+}
+Function Get-whoapi {
+    [cmdletbinding()]
+    [OutputType("whoapi")]
+    param(
+        [parameter(Position = 0,
+            Mandatory,
+            HelpMessage = "Enter an FQDN address to lookup with domainscore",
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        $fqdn
+    )
+    begin {
+        Write-Verbose "Function: $($MyInvocation.Mycommand)"
+        $apiname = 'WHOAPI'
+        $domain = $fqdn
+        $r = "whois"
+    }
+    Process {
+        if ($fqdn) {
+            Try {
+                
+                $apikey = "$whoapikey"
+                # https://api.whoapi.com/?domain=whoapi.com&r=whois&apikey=YOUR_API_KEY
+                $url = "https://api.whoapi.com/?domain=$fqdn&r=$r&apikey=$apikey"
+                $response = Invoke-WebRequest -Method Get -Uri "$url" -ContentType 'application/json'
+            }
+            Catch {
+                Write-error "$($_.Exception.Message)" 
+            }
+            if ($response) {
+                $t = $response.Content | ConvertFrom-Json
+                $script:tx = $t.results
+                $apiname | Trace-word -words 'WHOAPI'
+                if ([switch]$raw) {
+                    $t
+                }
+                Else {
+                    $properties = ($t | Get-Member -MemberType Properties).Name
+                    [hashtable]$table = @{
+                        PSTypeName = "WHOAPI"
+                    }
+                    ForEach ($property in $properties) {
+                       #If ($t."$property" -eq $t.results) {
+                            If ($t."$property") {
+                                $n = $property + ": " + $t."$property" 
+                                $table.Add($property, $t."$property")
+                                Write-log " [WHOAPI] $n"
+                            }
+                       #}
+                    }
+                }
+            }
+        }
+    }
+    End {
+        if ($table) {
+            $htable = New-Object -TypeName psobject -Property $table
+            $htable | Format-List
+        }
+        $tx
+        Write-log " [WHOAPI] $($tx)"
+        Write-Verbose "Exiting $($MyInvocation.Mycommand)"
+    }
+}
+Function Get-whoblacklist {
+    [cmdletbinding()]
+    [OutputType("whoapi")]
+    param(
+        [parameter(Position = 0,
+            Mandatory,
+            HelpMessage = "Returns all services that have been found on the given host IP.",
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [ValidatePattern("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")]    
+        $ipaddress
+    )
+    begin {
+        Write-Verbose "Function: $($MyInvocation.Mycommand)"
+        $apiname = 'WHOAPI'
+        # $domain = $fqdn
+        $r = "blacklist"
+    }
+    Process {
+        if ($ipaddress) {
+            Try {
+                
+                $apikey = "$whoapikey"
+                # https://api.whoapi.com/?apikey=YOUR_API_KEY&r=blacklist&domain=whoapi.com&ip=
+                $url = "https://api.whoapi.com/?apikey=$apikey&r=$r&domain=whoapi.com&ip=$ipaddress"
+                $response = Invoke-WebRequest -Method Get -Uri "$url" -ContentType 'application/json'
+            }
+            Catch {
+                Write-error "$($_.Exception.Message)" 
+            }
+            if ($response) {
+                $t = $response.Content | ConvertFrom-Json
+                $script:tx = $t.blacklists
+                $apiname | Trace-word -words 'WHOAPI'
+                if ([switch]$raw) {
+                    $t
+                }
+                Else {
+                    $properties = ($t | Get-Member -MemberType Properties).Name
+                    [hashtable]$table = @{
+                        PSTypeName = "WHOAPI"
+                    }
+                    ForEach ($property in $properties) {
+                       #If ($t."$property" -eq $t.results) {
+                            If ($t."$property") {
+                                $n = $property + ": " + $t."$property" 
+                                $table.Add($property, $t."$property")
+                                Write-log " [WHOAPI] $n"
+                            }
+                       #}
+                    }
+                }
+            }
+        }
+    }
+    End {
+        if ($table) {
+            $htable = New-Object -TypeName psobject -Property $table
+            $htable | Format-List
+        }
+        $tx
+        Write-log " [WHOAPI] $($tx)"
         Write-Verbose "Exiting $($MyInvocation.Mycommand)"
     }
 }
